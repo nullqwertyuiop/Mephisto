d34721bfaf684436bad66a6be8ba280d209abfef
nullqwertyuiop <null@member.fsf.org>
2024-07-09 14:58:54 +0800

    feat: support disabling richuru

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ def setup_env():
     os.environ["MEPHISTO_TRACE"] = "1" if args.trace else "0"
     os.environ["MEPHISTO_NO_CONSOLE"] = "1" if args.no_console else "0"
     os.environ["MEPHISTO_NO_DAEMON"] = "1" if args.no_daemon else "0"
+    os.environ["MEPHISTO_NO_RICHURU"] = "1" if args.no_richuru else "0"
     os.environ["MEPHISTO_DAEMON_PORT"] = str(args.daemon_port)
 
 
@@ -33,6 +34,7 @@ if __name__ == "__main__":
     parser.add_argument("--trace", action="store_true")
     parser.add_argument("--no-console", action="store_true")
     parser.add_argument("--no-daemon", action="store_true")
+    parser.add_argument("--no-richuru", action="store_true")
     parser.add_argument("--daemon-port", type=int, default=0)
     args = parser.parse_args()
 
--- mephisto/shared/util.py
+++ mephisto/shared/util.py
@@ -11,18 +11,21 @@ from rich.theme import Theme
 def setup_logger(sink: Path, log_rotate: int):
     level = "DEBUG" if os.environ["MEPHISTO_DEBUG"] == "1" else "INFO"
     level = "TRACE" if os.environ["MEPHISTO_TRACE"] == "1" else level
-    richuru.install(
-        rich_console=Console(
-            theme=Theme(
-                {
-                    "logging.level.success": "green",
-                    "logging.level.debug": "magenta",
-                    "logging.level.trace": "bright_black",
-                }
-            )
-        ),
-        level=level,
-    )
+    if os.environ["MEPHISTO_NO_RICHURU"] == "1":
+        logger.info("[Logger] Richuru disabled")
+    else:
+        richuru.install(
+            rich_console=Console(
+                theme=Theme(
+                    {
+                        "logging.level.success": "green",
+                        "logging.level.debug": "magenta",
+                        "logging.level.trace": "bright_black",
+                    }
+                )
+            ),
+            level=level,
+        )
     logger.add(
         sink / "{time:YYYY-MM-DD}" / "common.log",
         level="INFO",
